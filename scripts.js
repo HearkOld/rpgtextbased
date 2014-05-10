2b85a146c859f97275969b12afdb6c9f9928a8c7
Heark <hearkofsorcerey@gmail.com>
2014-05-10 16:23:45 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,21 +8,26 @@ var name = prompt("Please enter your name", "Lame Warrior");
  var off = false
  var userclass = prompt(name + "Please enter your class", " ");
  classchoose = on
- if(classchoose == on)
+ if(classchoose == on){
  document.write(" Classes: Paladin, Mage, and Cleric.")
- if(userclass == "Paladin")
- document.write("You're a Paladin!")
- if(userclass == "Mage")
- document.write("You're a Mage!")
- if(userclass == "Cleric")
- document.write("You're a Cleric!")
- else
- document.write("Class doesn't exist! Please try again.")
- prompt(name + ", Please enter your class", " ");
+ 
+ else if(userclass == "Paladin"){
+  document.write("You're a Paladin!");
+ }
+ 
+ else if(userclass == "Mage"){
+  document.write("You're a Mage!");
+ }
+ 
+ else if(userclass == "Cleric"){
+  document.write("You're a Cleric!");
+ }
+ else{
+  document.write("Class doesn't exist! Please try again.");
+  prompt(name + ", Please enter your class", " ");
 
 classchoose = off
 var race = prompt("Please enter your race", "Human");
 var gender = prompt("Please enter your gender", "Male");
 document.write("So you are " + name + " the " + gender + " " + race + " " + userclass + ".")
-
 // Enemy Creation
