95d2d03ea6a0e770d8aa475dd6a0f8dcd40d2690
Heark <hearkofsorcerey@gmail.com>
2014-05-10 15:21:40 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,15 +6,21 @@ var name = prompt("Please enter your name", "Lame Warrior");
  var classchoose = alert("Please choose your class")
  var on = true
  var off = false
- var choice1 = prompt(name + "Please enter your class", " ");
+ var class = prompt(name + "Please enter your class", " ");
  classchoose = on
  if(classchoose == on)
  document.write(" Classes: Paladin, Mage, and Cleric.")
- if(choice1 == "Paladin")
+ if(class == "Paladin")
  document.write("You're a Paladin!")
+ if(class == "Mage")
+ document.write("You're a Mage!")
+ if(class == "Cleric")
+ document.write("You're a Cleric!")
  else
  document.write("Class doesn't exist! Please try again.")
  prompt(name + ", Please enter your class", " ");
-end
 
  classchoose = off
+var race = prompt("Please enter your race", "Human");
+var gender = prompt("Please enter your gender", "Male");
+document.write("So you are " + name + " the " + gender + "" + race + "" + class + ".")
