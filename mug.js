6820e49fc70efd48896f5a751fc67c67768be58c
Heark <hearkofsorcerey@gmail.com>
2014-05-10 15:57:10 -0400

    Create mug.js

---
+++ mug.js
@@ -0,0 +1,12 @@
+var myAge = prompt("What is your age?");
+if(myAge>=14){
+  confirm("You may pass...");
+}
+else{
+  alert("You may play, but I am not responsible for your actions during or after the game, get your parents permission first");
+}
+alert("Welcome to Mug's Galaxy!");
+alert("The choose your own RPG game!");
+confirm("Are you ready?");
+alert("Good let's begin!");
+alert("First off, What is your name?")
