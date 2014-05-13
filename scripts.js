7b70723406453345552530368dc564c1eb830b39
Heark <hearkofsorcerey@gmail.com>
2014-05-13 17:30:37 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,10 +6,17 @@
 // Thank you For VIEWING!
 // Feel free to fork and pull request.
 
+
 // Character Creation
 var name = prompt("Please enter your name", "Lame Warrior");
+if(name.length <= 10)
+alert("Name too long! Ended Session. Please refresh.")
+end
+
 var message = document.write
  message("Hi " + name + "!" )
+
+
 
 
  var classchoose = alert("Please choose your class")
@@ -26,37 +33,46 @@ var message = document.write
  
  else if(userclass == "Mage"){
   message("You're a Mage!");
+  var race = prompt("Please enter your race", "Human");
+var gender = prompt("Please enter your gender", "Male");
+ message("So you are " + name + " the " + gender + " " + race + " " + userclass + ".")
  }
  
  else if(userclass == "Cleric"){
   message("You're a Cleric!");
+  var race = prompt("Please enter your race", "Human");
+var gender = prompt("Please enter your gender", "Male");
+ message("So you are " + name + " the " + gender + " " + race + " " + userclass + ".")
  }
  else{
   message("Class doesn't exist! Please try again.")
   var userclass = prompt(name + "Please enter your class", " ");
   
-
 classchoose = off
+
 var race = prompt("Please enter your race", "Human");
 var gender = prompt("Please enter your gender", "Male");
  message("So you are " + name + " the " + gender + " " + race + " " + userclass + ".")
 
+
 // Skills
 
+
 var skills = this;
-if(userclass == "Paladin")
-var skill = Array["slice", "punch", "attack", "kick", "run", "Special"];
+if(userclass == "Paladin"){
+var skill = Array["slice", "punch", "attack", "kick", "run","Special"];
 var basehp = 12
 var basedef = 15
 var baseagi = 20
 var basemag = 0
+}
 
-else if(userclass == "Mage")
+else if(userclass == "Mage"){
 var skillM = Array["Punch", "Magic", "Run", "Special"]
 var basehp = 14
 var basedef = 10
 var baseagi = 13
 var basemag = 25
-
 }
 
+}
