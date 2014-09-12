dd8a3542b98f7f3529f68964f99e18414bf36f82
Heark <hearkofsorcerey@gmail.com>
2014-09-12 11:56:47 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -5,6 +5,6 @@
 // (C)opyright Heark Gaming Co.
 //*************************************************************************
 var maps = this;
-var mapmoveablepoints = [1, 1, 1, 1, 1,
+var mapmoveablepoints = new.Array[1, 1, 1, 1, 1,
                         2, 2, 2, 2, 2, 3,
                         3, 3, 3, 5, 4, 4, 5]
