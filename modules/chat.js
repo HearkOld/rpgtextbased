745b5622cbb6233cdfdd567d773237445b769f2a
Heark <hearkofsorcerey@gmail.com>
2014-09-18 22:26:41 -0400

    Update chat.js

--- modules/chat.js
+++ modules/chat.js
@@ -1,3 +1,10 @@
 // Chat Module
+
 var msg = console.log
-var start = function(start)
+var start = function(start) {
+  var loadornah = prompt("Load?")
+  if (loadornah == "yes")
+  get.Data("Heark/repgtextbased/master/modules/chat.js")
+  
+}
+
