c9b51582eaac22ac7640a2ce5c369afb568748ae
Heark <hearkofsorcerey@gmail.com>
2014-09-29 11:46:07 -0400

    Update paladin.js

--- classes/paladin.js
+++ classes/paladin.js
@@ -1,1 +1,17 @@
 var physData = this;
+
+var stats = {
+    spA === 6
+    Att === 10
+    Def === 11
+    spD === 7
+}
+
+var basestats = {
+    this.spA
+    this.Att
+    this.Def
+    this.spD
+}
+
+var Physimage = ("http://img2.wikia.nocookie.net/__cb20120623113820/berserkrebirth/ru/images/6/66/Obraz_paladin.png")
