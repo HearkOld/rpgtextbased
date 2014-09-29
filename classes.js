6f8a472c35784acf73e20cf289e3ad3dbcadf47b
Heark <hearkofsorcerey@gmail.com>
2014-09-29 11:40:36 -0400

    Create classes.js

---
+++ classes.js
@@ -0,0 +1,17 @@
+var magicData = this;
+
+var stats = {
+    spA === 10
+    Att === 6
+    Def === 7
+    spD === 11
+}
+
+var Magicbasestats = {
+    this.spA
+    this.Att
+    this.Def
+    this.spD
+}
+
+var Magicimage = ("http://cdn.thebasementwizard.com/images/wizard.png")
