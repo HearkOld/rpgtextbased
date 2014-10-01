19d58899bb2f2535d862db0692eb44aae56bd658
Heark <hearkofsorcerey@gmail.com>
2014-10-01 10:31:01 -0400

    Update mapspartone.js

--- mapspartone.js
+++ mapspartone.js
@@ -8,4 +8,6 @@ var maps = this;
 var mapmoveablepoints = [1, 1, 1, 1, 1,
                         2, 2, 2, 2, 2, 3,
                         3, 3, 3, 5, 4, 4, 5]
-
+var move = function(move){
+    get(mapmoveablepoints);
+}
