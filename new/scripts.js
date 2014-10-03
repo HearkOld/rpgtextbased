c4705737e91c82462c711bbf8d582cf441e74816
Heark <hearkofsorcerey@gmail.com>
2014-10-03 11:57:44 -0400

    Update scripts.js

--- new/scripts.js
+++ new/scripts.js
@@ -1,15 +1,25 @@
-var stats = function(){
-var health = 0
-var attack = 0
-var defense = 0
-var specialattack = 0
-var specialdefense = 0
-}
+// info gather
+var name = prompt("what is your name?")
+var classmessage = alert("Availiable classes are: Paladin, Mage, Marksman")
+var userclass = prompt("Please choose your class.")
 
+// creating players
 var player = function(basehp, baseatt, basedef, basespa, basespd){
     this.basehp = health;
     this.baseatt = attack;
     this.basedef = defense;
     this.basespa = specialattack;
     this.basespd = specialdefense;
+}
+var move = function(damage, accuracy){
+    this.damage = damage;
+    this.accuracy = accuracy;
+}
+// creating the skills
+var punch = new move(10,100)
+var kick = new move(15,85)
+// class creation
+if (userclass == "Paladin"){
+    var user = new player(100, 12, 10, 5, 6)
+    var skills = [punch, kick]
 }
