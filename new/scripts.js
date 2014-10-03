fa68d58911835f40cf0114a1b6770cbaf50247c3
Heark <hearkofsorcerey@gmail.com>
2014-10-03 11:25:47 -0400

    Create scripts.js

---
+++ new/scripts.js
@@ -0,0 +1,15 @@
+var stats = function(){
+var health = 0
+var attack = 0
+var defense = 0
+var specialattack = 0
+var specialdefense = 0
+}
+
+var player = function(basehp, baseatt, basedef, basespa, basespd){
+    this.basehp = health;
+    this.baseatt = attack;
+    this.basedef = defense;
+    this.basespa = specialattack;
+    this.basespd = specialdefense;
+}
