49a5866e2ff1bf3c0b50cbaa2a8f11ac49affe90
Heark <hearkofsorcerey@gmail.com>
2014-10-07 11:13:14 -0400

    Update scripts.js

--- new/scripts.js
+++ new/scripts.js
@@ -38,6 +38,9 @@ var enemy = function(health, attack, defense, specialattack, specialdefense){
     this.specialdefense = specialdefense;   
 }
 var evilcat = new enemy(40, 3, 4, 3, 4)
+var dragon = new enemy(50, 5, 4, 3, 4)
+var orc = new enemy(10, 1, 1, 1, 1)
+var ogre = new enemy(25, 3, 4, 4, 5)
 // creating the skills
 var punch = new move(10,100)
 var kick = new move(15,85)
@@ -52,6 +55,80 @@ else if (userclass == "Mage"){
     var user = new player(100, 6, 6, 14, 11)
     var skills = [wisp, minorheal]
 }
+// names
+var names;
 
-// attack script
-damage + attack
+// battle mod
+var randnum = Math.floor(Math.random(10)*10);
+
+// battle script
+// Part 1
+//selected monster
+var selectedmonster;
+if (randomnumber >= 0 && randomnumber < 3){
+selectedmonster = evilcat;
+names = "Evil Cat";
+}
+else if (randomnumber > 3 && randomnumber < 6){
+selectedmonster = dragon;
+names = "dragon";
+}
+else if (randomnumber > 6){
+selectedmonster = orc;
+names = "orc";
+}
+else if (randomnumber > 9){
+selectedmonster = ogre;
+names = "ogre";
+}
+// Part 2
+var battle = 1;
+var monsterdead = 0;
+var playerdead = 0;
+
+
+// player first, player turn =1 
+var playerturn = 1;
+var monsterturn = 0;
+
+
+while (battle == 1) {
+//my turn
+while (playerturn == 1) {
+//attack script
+selectedmonster.health = selectedmonster.health - Math.ceil(Math.random(player.attack)*10);
+console.log(names + "'s Health is now " + selectedmonster.health);
+playerturn = 0;
+monsterturn = 1;
+}
+
+
+//monster turn
+
+
+while (monsterturn ==1) {
+//attack script
+player.health = player.health - Math.ceil(Math.random(selectedmonster.attack)*10);
+console.log("Player's Health is now " + player.health);
+playerturn = 1;
+monsterturn = 0;
+}
+
+
+//when the opponent dies
+if (selectedmonster.health < 0) {
+monsterdead = 1;
+battle = 0;
+}
+
+
+//if I die... lolz
+
+
+if (player.health < 0) {
+playerdead = 1;
+battle = 0;
+}
+}
+
+
