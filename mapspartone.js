773b33405f811ff71152f724fb97ad1efc2a74e4
Heark <hearkofsorcerey@gmail.com>
2014-10-08 11:30:37 -0400

    Update mapspartone.js

--- mapspartone.js
+++ mapspartone.js
@@ -4,10 +4,13 @@
 // Authored on 9/12/2014
 // (C)opyright Heark Gaming Co.
 //*************************************************************************
+data();
 var maps = this;
 var mapmoveablepoints = [1, 1, 1, 1, 1,
                         2, 2, 2, 2, 2, 3,
                         3, 3, 3, 5, 4, 4, 5]
-var move = function(move){
-    get(mapmoveablepoints);
+var move = function(distance, time){
+                        distance = this.distance;
+                        time = this.time;
 }
+if input = "/move"
