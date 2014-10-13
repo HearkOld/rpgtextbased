d9a2ff59008113ce393409d790fc9814909b391f
Heark <hearkofsorcerey@gmail.com>
2014-10-13 11:38:00 -0400

    Update ranks.js

--- ranks.js
+++ ranks.js
@@ -1,1 +1,1 @@
-var ranks;
+var ranks = [Private, Private First Class, Cadet, Cadet First Class, Sergeant, Sergeant First Class, Colonel, Colonel First Class, Major, "Major First Class", General Major];
