d7b4ae0643db5bebc5a8ac95da2a6dc83bd99489
Heark <hearkofsorcerey@gmail.com>
2014-10-13 11:41:56 -0400

    Update ranks.js

--- ranks.js
+++ ranks.js
@@ -1,1 +1,9 @@
-var ranks = [Private, Private First Class, Cadet, Cadet First Class, Sergeant, Sergeant First Class, Colonel, Colonel First Class, Major, "Major First Class", General Major];
+var ranks = [Private, Private First Class, Cadet, Cadet First Class, Sergeant, Sergeant First Class, Colonel, Colonel First Class, Major, "Major First Class", General, General Major
+];
+var exp = this.exp
+if exp <= 500
+userrank = Private
+if exp <= 1250
+userrank <= Private First Class
+if exp <= 2750 
+userrank = Cadet
